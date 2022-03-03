138e4380a60bf8d1fe6e18351068c8c31128e891
chamikaCN <chamikanandasiri97@gmail.com>
2022-03-03 23:52:53 +0530

    add : execute method implemented for heart beats received

--- src/main/java/serverserver/command/followertoleader/HeartbeatF2LCommand.java
+++ src/main/java/serverserver/command/followertoleader/HeartbeatF2LCommand.java
@@ -8,11 +8,14 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import serverserver.command.leadertofollower.CheckIdentityL2FCommand;
+import state.StateManager;
+import state.StateManagerImpl;
 
 @Getter
 @Setter
 public class HeartbeatF2LCommand extends ExecutableCommand {
     private static final Logger LOGGER = LoggerFactory.getLogger(HeartbeatF2LCommand.class);
+    private static final StateManager STATE_MANAGER = StateManagerImpl.getInstance();
 
     private String from;
 
@@ -28,6 +31,7 @@ public class HeartbeatF2LCommand extends ExecutableCommand {
     @Override
     public Command execute() {
         LOGGER.debug("Executing Heartbeat from Server {}", from);
+        STATE_MANAGER.getLeader().getHeartbeatDetector().updateTime(from,System.currentTimeMillis());
         return this;
     }
 }
