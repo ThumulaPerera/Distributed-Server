dcc86cefdce93ce386099455f238519124001a6b
chamikaCN <chamikanandasiri97@gmail.com>
2022-03-03 15:55:34 +0530

    add: HeartbeatPulser class to send heartbeats

---
+++ src/main/java/serverserver/HeartbeatPulser.java
@@ -0,0 +1,73 @@
+package serverserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.JsonParser;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * This thread is responsible to handle client connection. * * @author www.codejava.net
+ */
+public class HeartbeatPulser {
+    private static Logger LOGGER = LoggerFactory.getLogger(HeartbeatPulser.class);
+    private static final ObjectMapper MAPPER = JsonParser.getMapper();
+
+    private String address;
+    private int port;
+
+    public HeartbeatPulser(String address, int port) {
+        this.address = address;
+        this.port = port;
+    }
+
+    public void initiatePulse() {
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    if (!Objects.equals(Config.getServerId(), Config.getLeaderServerId())) {
+                        Socket socket = new Socket(address, port);
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                        sendPulse(bw);
+                        socket.close();
+                    }
+                } catch (IOException ex) {
+                    System.out.println("serverserver.Server exception: " + ex.getMessage());
+                    ex.printStackTrace();
+                }
+            }
+        }, 0, 5000);
+//            sendPulse(bw);
+//            Thread.sleep(10000);
+//            sendPulse(bw);
+//            Thread.sleep(3100);
+//
+//            sendPulse(bw);
+
+
+    }
+
+    private void sendPulse(BufferedWriter bw) {
+        try {
+            if (!Objects.equals(Config.getServerId(), Config.getLeaderServerId())) {
+                LOGGER.debug("Sent HB message from " + Config.getServerId());
+                bw.write("{\"type\" : \"heartbeat\", \"from\" : \"" + Config.getServerId() + "\"}");
+                bw.newLine();
+                bw.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
