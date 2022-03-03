23ffbfb931fec9b18d47f3a2509b0a566daf2023
Thumula Perera <thumusteam@gmail.com>
2022-03-03 17:02:01 +0530

    Add server to server message sender

---
+++ src/main/java/serverserver/Sender.java
@@ -0,0 +1,52 @@
+package serverserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import command.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import serverserver.command.leadertofollower.L2FCommandFactory;
+import state.ServerModel;
+import state.StateManager;
+import state.StateManagerImpl;
+import utils.JsonParser;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Sender {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Sender.class);
+    private static final ObjectMapper MAPPER = JsonParser.getMapper();
+    private static final StateManager STATE_MANAGER = StateManagerImpl.getInstance();
+
+    public Command sendCommandToLeader(Command command) {
+        Command response = null;
+
+        if (command == null) {
+            LOGGER.error("Command is null");
+            return null;
+        }
+
+        ServerModel leader = STATE_MANAGER.getLeader();
+
+        try (Socket socket = new Socket(leader.getAddress(), leader.getCoordinationPort());
+             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true)
+        ) {
+            String sentMessage = MAPPER.writeValueAsString(command);
+            pw.println(sentMessage);
+            LOGGER.debug("Sending message to leader: " + sentMessage);
+
+            String receivedMessage;
+            receivedMessage = br.readLine();
+            LOGGER.debug("Received message from leader: " + receivedMessage);
+
+            response = L2FCommandFactory.createL2FCommand(receivedMessage);
+
+            LOGGER.debug("Closing connection to leader: {}", socket.getRemoteSocketAddress());
+        }catch(IOException e) {
+            System.out.println(e);
+        }
+
+        return response;
+    }
+}
