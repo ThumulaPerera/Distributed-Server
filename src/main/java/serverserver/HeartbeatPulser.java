4b1c72a7d84cc2b17e3cb8262de8c961f9855d02
chamikaCN <chamikanandasiri97@gmail.com>
2022-03-04 09:43:58 +0530

    refactor: introduce constant to pulse interval and redesign constructor

--- src/main/java/serverserver/HeartbeatPulser.java
+++ src/main/java/serverserver/HeartbeatPulser.java
@@ -21,15 +21,11 @@ import java.util.TimerTask;
  * This thread is responsible to handle client connection. * * @author www.codejava.net
  */
 public class HeartbeatPulser {
-    private static Logger LOGGER = LoggerFactory.getLogger(HeartbeatPulser.class);
+    private static final int HEARTBEAT_PULSE_INTERVAL = 5000;
+    private static final Logger LOGGER = LoggerFactory.getLogger(HeartbeatPulser.class);
     private static final StateManager STATE_MANAGER = StateManagerImpl.getInstance();
 
-    private String address;
-    private int port;
-
-    public HeartbeatPulser(String address, int port) {
-        this.address = address;
-        this.port = port;
+    public HeartbeatPulser() {
     }
 
     public void initiatePulse() {
@@ -41,41 +37,9 @@ public class HeartbeatPulser {
                 if(!STATE_MANAGER.isLeader()) {
                     sendPulse(sender);
                 }
-//                try {
-//                    if (!Objects.equals(Config.getServerId(), Config.getLeaderServerId())) {
-//                        Socket socket = new Socket(address, port);
-//                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-//                        sendPulse(bw);
-//                        socket.close();
-//                    }
-//                } catch (IOException ex) {
-//                    System.out.println("serverserver.Server exception: " + ex.getMessage());
-//                    ex.printStackTrace();
-//                }
             }
-        }, 0, 5000);
-//            sendPulse(bw);
-//            Thread.sleep(10000);
-//            sendPulse(bw);
-//            Thread.sleep(3100);
-//
-//            sendPulse(bw);
-
-
+        }, 0, HEARTBEAT_PULSE_INTERVAL);
     }
-
-//    private void sendPulse(BufferedWriter bw) {
-//        try {
-//            if (!Objects.equals(Config.getServerId(), Config.getLeaderServerId())) {
-//                LOGGER.debug("Sent HB message from " + Config.getServerId());
-//                bw.write("{\"type\" : \"heartbeat\", \"from\" : \"" + Config.getServerId() + "\"}");
-//                bw.newLine();
-//                bw.flush();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     private void sendPulse(Sender sender) {
         sender.sendCommandToLeader(new HeartbeatF2LCommand(STATE_MANAGER.getSelf().getId()));
